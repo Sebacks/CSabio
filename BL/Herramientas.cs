5f90bc7fd6537a0b5c0870d79b2463727cf1847b
Sebastian Morales <gabriel69sebas@gmail.com>
2023-07-11 13:50:01 -0600

    another thinsgs

--- BL/Herramientas.cs
+++ BL/Herramientas.cs
@@ -38,10 +38,70 @@ namespace BL
             else
                 return false;
         }
-        #endregion
-        #region Validations
-        //valActual es para el valor actual de las acciones de x cosa, valMin es para el valor minimo que pueden tener las acciones de la x cosa
-        public int randomNumberValidate(int valActual, int valMin)
+        private string[] words()
+        {
+            string[] wordNumber = new string[2];
+            Random rn = new Random();
+            int num = rn.Next(0, 7);
+            bool isPositive = wordIsPositive();
+            if (wordIsPositive())
+            {
+                switch (num)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                }
+            }
+            else
+            {
+                switch (num)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                }
+            }
+            #endregion
+            #region Validations
+            //valActual es para el valor actual de las acciones de x cosa, valMin es para el valor minimo que pueden tener las acciones de la x cosa
+            public int randomNumberValidate(int valActual, int valMin)
         {
             int val = randomNumber(valActual,valMin);
             bool stop = true; ;
