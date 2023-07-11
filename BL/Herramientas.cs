efe6ecc31b75cdd5f8296ddbc2a6e6478792b5cc
Sebastian Morales <gabriel69sebas@gmail.com>
2023-07-11 12:44:44 -0600

    randomMethods

--- BL/Herramientas.cs
+++ BL/Herramientas.cs
@@ -8,6 +8,32 @@ namespace BL
 {
     public class Herramientas
     {
-       
+       public int randomNumber()
+        {
+            Random rn = new Random();
+            return rn.Next(-200,200);
+        }
+        public int randomNumber(bool opc)
+        {
+            if (opc)
+            {
+                Random rn = new Random();
+                return rn.Next(-20000, 0);
+            }
+            else 
+            {
+                Random rn = new Random();
+                return rn.Next(0, 20000);
+            }
+        }
+        public bool wordIsPositive()
+        {
+            Random rn = new Random();
+            if(rn.Next(0, 1) == 1)
+                return true;
+            else
+                return false;
+        }
+
     }
 }
